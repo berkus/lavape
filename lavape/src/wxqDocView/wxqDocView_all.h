b620368d924a8606c79c27d9a1de133e02fe935e
Klaus Guenther <guenthk@users.sourceforge.net>
2005-04-25 09:36:41 +0000

    no message

---
+++ lavape/src/wxqDocView/wxqDocView_all.h
@@ -0,0 +1,12 @@
+#ifndef _wxqDocView_PCH
+#define _wxqDocView_PCH
+
+#include "disco_all.h"
+
+#include "defs.h"
+#include "docview.h"
+#include "mdiframes.h"
+#include "setup.h"
+#include "wxExport.h"
+
+#endif
