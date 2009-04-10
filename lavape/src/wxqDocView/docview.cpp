f4f522425b7ba72178665659235cebf6c0efb9ef
Klaus Guenther <guenthk@users.sourceforge.net>
2009-04-10 15:12:20 +0000

    x

--- lavape/src/wxqDocView/docview.cpp
+++ lavape/src/wxqDocView/docview.cpp
@@ -2128,17 +2128,19 @@ Assistant::Assistant()
      if (proc->state() != QProcess::Running) {
        //QString app = QLibraryInfo::location(QLibraryInfo::BinariesPath) + QDir::separator();
        QString app = QString(".") + QDir::separator();
+       QStringList args;
  #if !defined(Q_OS_MAC)
          app += QLatin1String("assistant");
- #else
-         app += QLatin1String("Assistant.app/Contents/MacOS/Assistant");
- #endif
-
-         QStringList args;
          args << QLatin1String("-collectionFile")
              << QLatin1String("../doc/LavaPE.qhc")
              << QLatin1String("-enableRemoteControl");
-
+#else
+         //app += QLatin1String("Assistant.app/Contents/MacOS/Assistant");
+         app += QLatin1String("assistant");
+         args << QLatin1String("-collectionFile")
+             << QLatin1String("../doc/LavaPE.qhc")
+             << QLatin1String("-enableRemoteControl");
+#endif
          proc->start(app, args);
 
          if (!proc->waitForStarted()) {
