753e6b8968fbb3d27f55751ebe85a8939d69d098
guenthk <klausdguenther@gmail.com>
2009-11-21 17:26:07 +0100

    forceOverride

--- lavape/src/LavaExecs/Check.cpp
+++ lavape/src/LavaExecs/Check.cpp
@@ -4489,6 +4489,11 @@ bool FuncExpression::Check (CheckData &ckd)
         ((SynObject*)handle.ptr)->SetError(ckd,&ERR_CallObjInStatic);
         ok &= false;
       }
+      if (funcDecl->TypeFlags.Contains(forceOverride)) {
+
+        ((SynObject*)function.ptr)->SetError(ckd,&ERR_ForceOverCalled);
+        ok &= false;
+      }
       if (funcDecl->TypeFlags.Contains(isInitializer)
       && !flags.Contains(staticCall)) {
         ((SynObject*)function.ptr)->SetError(ckd,&ERR_ExplicitInitializerCall);
