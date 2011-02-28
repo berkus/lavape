8f23368de2cf9ca82c1930e8b9d2781e4529ca7d
Klaus D. Günther <klausdguenther@gmail.com>
2011-02-28 16:09:36 +0100

    sleep commented out

--- lavape/src/Lava/Lava.cpp
+++ lavape/src/Lava/Lava.cpp
@@ -76,7 +76,7 @@ int main( int argc, char ** argv ) {
   CLavaApp app(argc,argv);
 
   //QTest::qSleep(20000);
-  Sleep(20000);
+  //Sleep(20000);
 
   ExeDir = app.applicationDirPath();
 #ifdef WIN32
