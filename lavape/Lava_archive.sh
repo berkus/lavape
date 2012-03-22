e2bba71a7f6d1522b51a827d1f1bcbd580632bac
Klaus D. Günther <klausdguenther@gmail.com>
2012-03-22 12:35:17 +0100

    added/renamed

---
+++ lavape/Lava_archive.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#cd /opt
+tar -c -j \
+--exclude=bin/*.app \
+--exclude=bin/Lava\
+--exclude=bin/LavaPE \
+--exclude=bin/LPC \
+--exclude=bin/assistant \
+--exclude lib \
+--exclude doc/LavaPE.qch \
+--exclude doc/LavaPE.qhc \
+--exclude=doc/ftv2* \
+--exclude=doc/ftiens4.js \
+--exclude=doc/diff** \
+--exclude *.gch \
+--exclude=LavaWWW \
+--exclude='CVS*' \
+--exclude=Generated \
+--exclude=Makefile \
+--exclude=moc_* \
+--exclude=*.xcodeproj \
+--exclude=*.cur \
+--exclude=*.qhc \
+--exclude=*.qch \
+--exclude=qtc-* \
+--exclude=*.bmp \
+--exclude=*.xml \
+--exclude=*.ldat \
+--exclude=*.bash \
+--exclude=*.bat \
+--exclude=*.phd \
+--exclude=*.bz2 \
+--exclude=*.gz \
+--exclude=*.proj \
+--exclude='*~' \
+--exclude='*.o' \
+--exclude='*.so' \
+--exclude='*.dylib' \
+--exclude='*.d' \
+--exclude='*.jar' \
+--exclude='*.class' \
+--exclude='*.iss' \
+--exclude='.[#,a-z,A-Z]*' \
+-f lavape-0.9.2-src.tar.bz2 lavape
+ls -l lavape-0.9.2-src.tar.bz2
